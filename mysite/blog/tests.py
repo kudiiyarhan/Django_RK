e1bd718577fff0bfb7650e5cf1bbeef9c28a1b85
kudiiyarhan <romeo333.91@gmail.com>
2023-10-22 17:18:59 +0300

    Первый коммит

---
+++ mysite/blog/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from  django.urls import resolve
+from  .views import home_page
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        '''тест проверяющий что при переходе на главную страницу открывается view под названием home_page'''
+
+        found = resolve('/')
+        self.assertEquals(found.func, home_page)
